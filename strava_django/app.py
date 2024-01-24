5a9a3ee8f19187b5153dcc2dfa52e35765b9f369
mestre05053 <mestre05053@gmail.com>
2024-01-23 20:19:47 -0600

    Ya casi tengo lista la logica de la redireccion

--- strava_django/app.py
+++ strava_django/app.py
@@ -1,16 +1,44 @@
-import requests, json
+import requests
+import webbrowser
 from strava_django.entities.auth import Auth
+
+import os
+import binascii
+import urllib.parse
+
+from http.server import BaseHTTPRequestHandler
+from http.server import HTTPServer
 
 auth = Auth()
 #token = auth.get_token()
 # TO DO esta puesto esto aqui para evitar que de error con los headers
 token = 0
 
+state = binascii.hexlify(os.urandom(20)).decode('utf-8')
+
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        global code
+        self.close_connection = True
+        query = urllib.parse.parse_qs(urllib.parse.urlparse(self.path).query)
+        if not query['state'] or query['state'][0] != state:
+            raise RuntimeError("state argument missing or invalid")
+        code = query['code']
+
 def test():
     oauth_url = auth.sacar_afuera()
-    print(oauth_url)
-    response = requests.post(oauth_url)
-    print(response)
+    response = requests.get(oauth_url)
+    print(response.status_code)
+    #webbrowser.open_new_tab(oauth_url)
+    
+    server = HTTPServer(('127.0.0.1', 8081), RequestHandler)
+    server.handle_request()
+
+    ###TO DO 
+    '''
+    La logica es que el servidor que levanto sea a al que se le envie la redireccion se strava
+    luego de la url de la redireccion de strava carga el code.
+    '''
 
 def get_user_data():
 
